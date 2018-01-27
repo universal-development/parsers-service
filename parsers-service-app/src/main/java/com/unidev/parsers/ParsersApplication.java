b8ac2601332c6275c29b004ae8bcc79a16cc3c6e
Denis O <denis@universal-development.com>
2018-01-27 22:26:03 +0200

    Added template project

---
+++ parsers-service-app/src/main/java/com/unidev/parsers/ParsersApplication.java
@@ -0,0 +1,12 @@
+package com.unidev.parsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsersApplication.class, args);
+	}
+}
